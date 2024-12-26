34b8c20d975660d74e7bafd180de0b4a13b9e108
Franklin Menezes Pereira <fmpereira2309@hotmail.com>
2024-12-26 09:46:41 -0300

    criação do menu de opções

--- src/conversorMassa.c
+++ src/conversorMassa.c
@@ -17,3 +17,37 @@ void converterParaToneladas(double valor) {
     printf("Valor em gramas: %.2f g\n", valor * 1000000); 
     printf("Valor em toneladas: %.2f t\n", valor); 
 } 
+
+int main() { 
+    int opcao; double valor; 
+    
+    do { 
+        printf("\n=== Conversor de Unidades de Massa ===\n"); 
+        printf("1. Converter de Quilograma\n"); 
+        printf("2. Converter de Grama\n"); 
+        printf("3. Converter de Tonelada\n"); 
+        printf("4. Sair\n"); 
+        printf("Escolha uma opcao: "); 
+        scanf("%d", &opcao); 
+
+        if (opcao == 4) { 
+            printf("Saindo...\n"); 
+            break; 
+        } 
+        
+        printf("Digite o valor a ser convertido: "); 
+        scanf("%lf", &valor); 
+        
+        switch (opcao) { 
+            case 1: converterParaQuilogramas(valor); 
+                break; 
+            case 2: converterParaGramas(valor); 
+                break; 
+            case 3: converterParaToneladas(valor); 
+                break; 
+            default: printf("Opcao invalida! Tente novamente.\n"); 
+        } 
+    } while (opcao != 4); 
+    
+    return 0;
+}
